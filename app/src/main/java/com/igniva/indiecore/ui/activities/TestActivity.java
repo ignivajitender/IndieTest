3151debcddcca13e9665dab2c382dea829a75d64
ignivasidharth <sidharth.github@ignivasolutions.com>
2016-06-14 12:52:22 +0530

    on 14jun

--- app/src/main/java/com/igniva/indiecore/ui/activities/TestActivity.java
+++ app/src/main/java/com/igniva/indiecore/ui/activities/TestActivity.java
@@ -30,7 +30,8 @@ public class TestActivity extends BaseActivity {
         setDataInViewObjects();
         // this is sidharth
         // this is sidharth 2
-        // this is sidharth 3
+
+        // this is sidharth 4
     }
 
 
