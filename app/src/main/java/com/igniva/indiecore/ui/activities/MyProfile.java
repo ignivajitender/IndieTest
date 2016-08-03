f79a31bd09930983c7696df4e477b493559d841c
ignivasidharth <sidharth.github@ignivasolutions.com>
2016-08-03 10:15:08 +0530

    on 3nd august @ 10:58..

---
+++ app/src/main/java/com/igniva/indiecore/ui/activities/MyProfile.java
@@ -0,0 +1,29 @@
+package com.igniva.indiecore.ui.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.igniva.indiecore.R;
+
+/**
+ * Created by igniva-andriod-05 on 2/8/16.
+ */
+public class MyProfile extends  BaseActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+
+    @Override
+    protected void setUpLayout() {
+
+    }
+
+    @Override
+    protected void setDataInViewObjects() {
+
+    }
+}
