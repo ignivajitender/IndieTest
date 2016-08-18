0da8bfd5b2bbf6797ffa578ed51c7ef55fe67edf
ignivajitender <jitneder.github@ignivasolutions.com>
2016-08-18 17:30:04 +0530

    bug fixing

--- app/src/main/java/com/igniva/indiecore/MyApplication.java
+++ app/src/main/java/com/igniva/indiecore/MyApplication.java
@@ -5,7 +5,16 @@ import android.app.Application;
 /**
  * Created by igniva-andriod-11 on 2/6/16.
  */
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
 public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fabric.with(this, new Crashlytics());
+    }
 
     private int appRunCount = 0;
 
